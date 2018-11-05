c3777db9dddccdd9aed27bb2957622949938f432
Dakota Clark <bannaray95@gmail.com>
2018-11-05 00:52:28 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,12 @@ def monopoly_with_third_tier
     :railroads =>
     {
       :pieces => 4,
-      :names => {},
+      :names => {
+        :reading_railroads => {},
+        :pennsylvania_railroads => {},
+        :b_and_o_railroads => {},
+        :shortline => {}
+      },
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_piece_owned => 50,
