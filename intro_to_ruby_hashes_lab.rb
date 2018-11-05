c414ca0f2232d6ab7d866b9a81f278f7ff8c392c
Dakota Clark <bannaray95@gmail.com>
2018-11-05 00:39:22 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,13 +11,12 @@ def monopoly
 
 end
 
-def monopoly_with_second_tier {
+def monopoly_with_second_tier
   monopoly = {railroads: ({}),
-  pieces: 4,
+  pieces: => 4,
   names: {},
   rent_in_dollars: {}
   }
-}
 end
 
 def monopoly_with_third_tier
